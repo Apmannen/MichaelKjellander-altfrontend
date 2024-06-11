23e1f2218b57c1026ab2911de9b316547033cd79
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-06-11 20:30:48 +0200

    #1; Sidebar progress

--- src/components/shells/MainShell/MainShell.tsx
+++ src/components/shells/MainShell/MainShell.tsx
@@ -1,9 +1,21 @@
 import { ReactNode } from "react";
+import cssClasses from "./MainShell.module.css";
 
 export function MainShell({ children }: { children: ReactNode }) {
 	return (
 		<>
-			<main>{children}</main>
+			<div className={`${cssClasses.container}`}>
+				<Sidebar />
+				<main className={`${cssClasses.main} pt-20`}>{children}</main>
+			</div>
+		</>
+	);
+}
+
+function Sidebar() {
+	return (
+		<>
+			<div className={`${cssClasses.sidebar} pt-20`}>Michael Kjellander</div>
 		</>
 	);
 }
