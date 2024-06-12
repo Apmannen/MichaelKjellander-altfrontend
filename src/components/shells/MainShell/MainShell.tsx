8eca29dcc9fbc041e4d13ad295400e1eb637b92f
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-06-12 02:06:45 +0200

    #2; Link to category pages

--- src/components/shells/MainShell/MainShell.tsx
+++ src/components/shells/MainShell/MainShell.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react';
 import css from './MainShell.module.css';
 import { fontNotoSans } from '@/components/styling/GlobalCssClasses/functions';
 import { apiService } from '@/modules/services/apiService';
+import { paths } from '@/modules/paths';
 
 export async function MainShell({ children }: { children: ReactNode }) {
   return (
@@ -25,7 +26,7 @@ async function Sidebar() {
         </a>
         <div className="h-20"></div>
         {categories.map((category, index) => (
-          <a className="block py-2" key={index} href="#">
+          <a className="block py-2" key={index} href={paths.pages.category(category.slug)}>
             {category.name}
           </a>
         ))}
---
+++ src/modules/paths/pages/index.ts
@@ -0,0 +1,3 @@
+export function category(slug: string): string {
+  return '/kategori/' + slug;
+}
